22eebdea4f68fbce0b1a920771b7e8cd991ae7c5
Vedant Bisen <bisenvedant@gmail.com>
2023-02-10 19:24:24 +0530

    main

--- ToDoList/ToDoApp/views.py
+++ ToDoList/ToDoApp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList
 
 # Create your views here.
+def base(response, id):
+    ls = ToDoList.objects.get(id=id)    
+    return render(response, "main/base.html", {})
 
+def home(response):
+    return render(response, "main/home.html", {})
