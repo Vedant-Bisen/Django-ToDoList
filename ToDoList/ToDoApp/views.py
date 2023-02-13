fe9cdd5c0facb12ae92e7b4b64cda841130dfe98
Vedant Bisen <bisenvedant@gmail.com>
2023-02-13 22:36:18 +0530

    main

--- ToDoList/ToDoApp/views.py
+++ ToDoList/ToDoApp/views.py
@@ -5,7 +5,7 @@ from .models import ToDoList
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)    
-    return render(response, "main/base.html", {"name":ls.name})
+    return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
-    return render(response, "main/home.html", {"name":"test"})
+    return render(response, "main/home.html",{})
